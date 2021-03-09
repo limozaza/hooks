ba0b5bda93e95c86309e9d11bed3e32c5f89d57a
limozaza <zakboite@gmail.com>
2021-03-09 21:35:08 +0100

    add props + semantic ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+
+const items = [
+  {
+    title: 'What is React ?',
+    content: 'React is FRONT end js framework'
+  },
+  {
+    title: 'Why use React ?',
+    content: 'React is my favorite FRONT end js framework'
+  },
+  {
+    title: 'Who do you useReact ?',
+    content: 'React is my good'
+  }
+];
+
 
 export default  () =>{
-  return <h1>Salam alikoum</h1>
+  return <div>
+    <Accordion items={items}/>
+  </div>
 }
 
