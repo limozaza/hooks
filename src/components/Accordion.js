af863bd69306ecc4e07497fb689e0f428cbb875d
limozaza <zakboite@gmail.com>
2021-03-09 21:55:10 +0100

    dynamic style class

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -14,22 +14,23 @@ const Accordion = ({items}) => {
 
 
     const renderItems = items.map((item,index)=>{
+        const active = index ===activeIndex ? 'active' : '';
+
         return <React.Fragment key={item.title}> 
                 <div 
-                className="title active"
+                className={"title "+active}
                 onClick={()=>onTitleClick(index)}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
     });
     return <div className="ui styled accordion">
         {renderItems}
-        <h1>{activeIndex}</h1>
     </div>
 };
 
